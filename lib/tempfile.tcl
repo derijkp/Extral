f395de31d1bd1c33fae7cfaf789e542794524809
Peter.DeRijk <Peter.DeRijk@ua.ac.be>
2006-12-05 14:09:55 +0000

    tempdir and tempfile change: all temp files in one dir,  tempfile returns names (files are not made) , ...

--- lib/tempfile.tcl
+++ lib/tempfile.tcl
@@ -13,39 +13,27 @@ namespace eval Extral {
 	# Make one if one doesn't exist.
 	# 
 	
-	if [info exists env(TEMP)] {
+	if {[info exists env(TEMPDIR)]} {
+		set temp_dir $env(TEMPDIR)
+	} elseif {[info exists env(TEMP)]} {
 		set temp_dir $env(TEMP)
-	} elseif [file writable /usr/tmp] {
+	} elseif {[file writable /usr/tmp]} {
 		set temp_dir /usr/tmp
-	} elseif [info exists env(HOME)] {
+	} elseif {[info exists env(HOME)]} {
 		set temp_dir [file join $env(HOME) tcltemp]
 	} else {
 		set temp_dir [file join [pwd] tcltemp]
 	}
 	
-	if ![file exists $temp_dir] {
+	if {![file exists $temp_dir]} {
 		if [catch {file mkdir $temp_dir}] {
 			puts stdout "Didn't find temporary directory"
 			puts stdout "Couldn't make temporary directory \"$temp_dir\""
 		}
-	} elseif ![file isdirectory $temp_dir] {
+	} elseif {![file isdirectory $temp_dir]} {
 		puts stdout "Didn't find temporary directory"
 		puts stdout "Temporary directory \"$temp_dir\" exists, and is not a directory"
 	}
-	
-	# Set lock
-	set num 1
-	while 1 {
-		set file [file join $temp_dir tempExtral$num.lck]
-		if ![file exists $file] break
-		incr num
-	}
-	set f [open $file w]
-	puts $f "lock"
-	close $f
-	set templock tempExtral$num 
-	set tempnum 0
-	catch {file delete -force [file join $temp_dir ${templock}_*]}
 	atexit add {tempfile clean}
 }
 
@@ -61,10 +49,9 @@ namespace eval Extral {
 #doc {tempfile get} cmd {
 #tempfile ?get? ?file|dir?
 #} descr {
-#	creates an (empty) temporary file and returns its name. You should remove
-#	the temporary file when not used any longer. However, leftover temporary 
-#	files will be removed by an atexit handler
-#	The last parameter (file or dir) determines whether a file or directory is created   
+#	Creates a temporary directory (using tempdir) if needed, and returns unique filenames
+#	in this directory for use as temporary files. The temporary directory is deleted when
+#	the program exits
 #}
 #doc {tempfile clean} cmd {
 #tempfile clean
@@ -77,28 +64,15 @@ namespace eval Extral {
 #	remove all temporary files, including those of other Extral programs.
 #}
 proc tempfile {{action {get}} {type file}} {
-	upvar ::Extral::temp_dir temp_dir
-	upvar ::Extral::templock templock
 	switch $action {
 		get {
-			set i 1
-			while 1 {
-				set file [file join $temp_dir ${templock}_$i]
-				if ![file exists $file] break
-				incr i
-			}
-			if {"$type" == "dir"} {
-				file mkdir $file
-			} else {
-				set f [open $file w]
-				puts $f ""
-				close $f
-			}
-			return $file
+			set tempdir [tempdir]
+			return [file join $tempdir _Extral_temp_[incr ::Extral::tempnum].tmp]
 		}
 		clean {
-			catch {eval file delete -force [glob [file join $temp_dir ${templock}_*]]}
-			catch {file delete -force [file join $temp_dir $templock.lck]}
+			set tempdir [tempdir]
+			file delete -force $tempdir
+			unset ::Extral::tempdir
 		}
 		cleanall {
 			catch {eval file delete -force [glob [file join $temp_dir tempExtral*]]}
@@ -109,19 +83,48 @@ proc tempfile {{action {get}} {type file}} {
 	}
 }
 
+proc Extral::randstring {size} {
+	set result {}
+	for {set i 0} {$i < $size} {incr i} {
+		append result [format %c [expr {int(rand() * 26) + [expr {int(rand() * 10) > 5 ? 97 : 65}]}]]
+	}
+	return $result
+}
+
 #doc tempdir title {
 #tempdir
 #} shortdescr {
 # returns a directory in which temporary files can be stored. This directory is specific to one proces:
 # no other processes will (should) write in this directory. Subsequent calls to the function within one process
 # will allways be the same directory, The program has to take care not to overwrite its own files
+# Temporary files returned by tempfile are also in this directory (named like _Extral_temp_1.tmp)
+# The program should also not overwrite these
+# The temporary directory is deleted when the program exits by an atexit handler
 # 
 #}
 
 proc tempdir {} {
-	upvar ::Extral::tempdir tempdir
-	if {![info exists tempdir]} {
-		set tempdir [tempfile get dir]
+	if {![info exists ::Extral::tempdir]} {
+		for {set i 0} {$i < 20} {incr i} {
+			set testdir [file join $::Extral::temp_dir tempExtral.[pid]-[Extral::randstring 20]]
+			if {[file exists $testdir]} continue
+			if {[catch {
+				file mkdir $testdir
+				if {$::tcl_platform(platform) eq "unix"} {
+					file attributes $testdir -permissions 0700
+				}
+				set files [glob -nocomplain $testdir/*]
+				if {[llength $files]} {
+					error "Very fishy: there are files in the temporary directory I just created"
+				}
+				set ::Extral::tempdir $testdir
+				set ::Extral::tempnum 0
+			}]} continue
+			break
+		}
 	}
-	return $tempdir
+	if {![info exists ::Extral::tempdir]} {
+		error "couldn't create temporary directory in [file nativename $::Extral::temp_dir]"
+	}
+	return $::Extral::tempdir
 }
