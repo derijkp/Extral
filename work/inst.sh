308eaf5c3e99ee76a2e9fc326ff8870c76b37917
Peter.DeRijk <Peter.DeRijk@ua.ac.be>
2009-04-30 13:32:38 +0000

    work/inst fixes: automatically set version, win install no z:

--- work/inst.sh
+++ work/inst.sh
@@ -1,10 +1,15 @@
+#!/bin/sh
+
+# $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
+export version=2.1.0
+
 # full compile and install linux
 cd /home/peter/dev/Extral/Linux-i686
 ../build/version.tcl
 make distclean
 ../configure --prefix=/home/peter/tcl/dirtcl
 make
-rm -rf /home/peter/build/tca/Linux-i686/exts/Extral2.1.0
+rm -rf /home/peter/build/tca/Linux-i686/exts/Extral$version
 /home/peter/dev/Extral/build/install.tcl /home/peter/build/tca/Linux-i686/exts
 
 # full cross-compile and install windows
@@ -12,11 +17,11 @@ cd /home/peter/dev/Extral/windows-intel
 make distclean
 cross-bconfigure.sh --prefix=/home/peter/tcl/win-dirtcl
 cross-make.sh
-rm -rf /home/peter/build/tca/Windows-intel/exts/Extral2.1.0
-wine /home/peter/build/tca/Windows-intel/tclsh84.exe /home/peter/dev/Extral/build/install.tcl /home/peter/build/tca/Windows-intel/exts
+rm -rf /home/peter/build/tca/Windows-intel/exts/Extral$version
+wine z:/home/peter/build/tca/Windows-intel/tclsh84.exe z:/home/peter/dev/Extral/build/install.tcl z:/home/peter/build/tca/Windows-intel/exts
 
 /home/peter/dev/Extral/build/version.tcl
 
 # install in httpd
-rm -rf /home/peter/dev/httpd/exts/Extral2.1.0
+rm -rf /home/peter/dev/httpd/exts/Extral$version
 /home/peter/dev/Extral/build/install.tcl /home/peter/dev/httpd/exts
--- work/quickinst.sh
+++ work/quickinst.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+
+# $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
+export version=2.1.0
+
 # quick install (lib only)
-rm -rf /home/peter/build/tca/Windows-intel/exts/Extral2.1.0/lib
-cp -r /home/peter/build/tca/Linux-i686/exts/Extral2.1.0/lib /home/peter/build/tca/Windows-intel/exts/Extral2.1.0/
+
+# quick install linux
+cd /home/peter/dev/Extral/Linux-i686
+../build/version.tcl
+rm -rf /home/peter/build/tca/Linux-i686/exts/Extral$version
+/home/peter/dev/Extral/build/install.tcl /home/peter/build/tca/Linux-i686/exts
+
+rm -rf /home/peter/build/tca/Windows-intel/exts/Extral$version/lib
+cp -r /home/peter/build/tca/Linux-i686/exts/Extral$version/lib /home/peter/build/tca/Windows-intel/exts/Extral2.1.0/
