92e697a9446327712c5b3b458084b7845fa453d4
Peter De Rijk <Peter.DeRijk@ua.ac.be>
2012-03-13 00:26:25 +0000

    inst changes: use new architecture dirnames

--- work/inst.sh
+++ work/inst.sh
@@ -1,27 +1,58 @@
 #!/bin/sh
+set -ex
 
 # $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
 export version=2.1.0
+name=Extral
 
 # full compile and install linux
-cd /home/peter/dev/Extral/Linux-i686
+echo "---------- Full install Linux-i686 to tca ----------"
+mkdir -p $HOME/dev/${name}/linux-ix86
+cd $HOME/dev/${name}/linux-ix86
+. $HOME/mybin/cross-compat-i686.sh
 ../build/version.tcl
-make distclean
-../configure --prefix=/home/peter/tcl/dirtcl
-make
-rm -rf /home/peter/build/tca/Linux-i686/exts/Extral$version
-/home/peter/dev/Extral/build/install.tcl /home/peter/build/tca/Linux-i686/exts
+make distclean || true
+PATH=$CROSSNBIN:$PATH ../configure --prefix=$DIRTCL
+PATH=$CROSSNBIN:$PATH make
+rm -rf $HOME/build/tca/Linux-i686/exts/${name}$version
+$HOME/tcl/dirtcl-i686/tclsh $HOME/dev/${name}/build/install.tcl $HOME/build/tca/Linux-i686/exts
+
+# full compile and install linux 64bit
+echo "---------- Full install Linux-x86_64 to tca ----------"
+mkdir -p $HOME/dev/${name}/linux-x86_64
+cd $HOME/dev/${name}/linux-x86_64
+. $HOME/mybin/cross-compat-x86_64.sh
+../build/version.tcl
+make distclean || true
+PATH=$CROSSNBIN:$PATH ../configure --prefix=$DIRTCL
+PATH=$CROSSNBIN:$PATH make
+rm -rf $HOME/build/tca/Linux-x86_64/exts/${name}$version
+$HOME/tcl/dirtcl-x86_64/tclsh $HOME/dev/${name}/build/install.tcl $HOME/build/tca/Linux-x86_64/exts
 
 # full cross-compile and install windows
-cd /home/peter/dev/Extral/windows-intel
-make distclean
-cross-bconfigure.sh --prefix=/home/peter/tcl/win-dirtcl
-cross-make.sh
-rm -rf /home/peter/build/tca/Windows-intel/exts/Extral$version
-wine z:/home/peter/build/tca/Windows-intel/tclsh84.exe z:/home/peter/dev/Extral/build/install.tcl z:/home/peter/build/tca/Windows-intel/exts
+echo "---------- Full install windows-intel to tca ----------"
+mkdir -p $HOME/dev/${name}/win32-ix86
+cd $HOME/dev/${name}/win32-ix86
+. $HOME/mybin/cross-compat-i386-mingw32msvc.sh
+../build/version.tcl
+make distclean || true
+PATH=$CROSSBIN:$PATH ../configure --prefix=$HOME/tcl/win-dirtcl --target=i386-mingw32msvc --host=i386-mingw32msvc --build=i386-linux
+PATH=$CROSSBIN:$PATH make
+rm -rf $HOME/build/tca/Windows-intel/exts/${name}$version
+wintclsh z:$HOME/dev/${name}/build/install.tcl z:$HOME/build/tca/Windows-intel/exts
 
-/home/peter/dev/Extral/build/version.tcl
+## full cross-compile and install windows
+#cd $HOME/dev/${name}/windows-intel
+#make distclean
+#cross-bconfigure.sh --prefix=$HOME/tcl/win-dirtcl
+#cross-make.sh
+#rm -rf $HOME/build/tca/Windows-intel/exts/${name}$version
+#wine z:$HOME/build/tca/Windows-intel/tclsh84.exe z:$HOME/dev/${name}/build/install.tcl z:$HOME/build/tca/Windows-intel/exts
+
+$HOME/dev/${name}/build/version.tcl
 
 # install in httpd
-rm -rf /home/peter/dev/httpd/exts/Extral$version
-/home/peter/dev/Extral/build/install.tcl /home/peter/dev/httpd/exts
+rm -rf $HOME/dev/httpd/exts/${name}$version
+$HOME/dev/${name}/build/install.tcl $HOME/dev/httpd/exts
+
+cd $HOME/dev/${name}
